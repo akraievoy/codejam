03807ac289bcda485d4fe5e4069cbad77bb56a08
akraievoy <akraievoy@gmail.com>
2018-08-23 08:28:37 +0100

    Squashed commit of the following:
    commit b44de0a5a8d6bf35217e6434e34fa557e2728ead
    Author: akraievoy <akraievoy@gmail.com>
    Date:   Tue Feb 6 18:26:12 2018 +0000
    
        revised russian roulette, 20 minutes

---
+++ src/hr_woc36_20_revised_russian_roulette/Solution_test.go
@@ -0,0 +1,27 @@
+package hr_woc36_20_revised_russian_roulette
+
+import (
+	"testing"
+)
+
+func TestSimple(t *testing.T) {
+	// FIXME simple test case
+	out := solve(In{[]bool{false, true, true, false, true, true, true, true, false, false}})
+
+	{
+		actual := out.movesMin
+		expected := 3
+		if actual != expected {
+			t.Errorf("out.movesMin is not correct: expected %v, actual %v", expected, actual)
+		}
+	}
+
+	{
+		actual := out.movesMax
+		expected := 6
+		if actual != expected {
+			t.Errorf("out.movesMax is not correct: expected %v, actual %v", expected, actual)
+		}
+	}
+
+}
