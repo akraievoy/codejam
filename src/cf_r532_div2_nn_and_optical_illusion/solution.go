f27d0473d950995c05ab44f5511c25b38265dd8a
akraievoy <akraievoy@gmail.com>
2019-01-17 23:08:23 +0000

    round #532 div 2 (cleanup)

--- src/cf_r532_div2_nn_and_optical_illusion/solution.go
+++ src/cf_r532_div2_nn_and_optical_illusion/solution.go
@@ -8,48 +8,6 @@ import (
 	"math"
 )
 
-type caseInput struct {
-	// FIXME test case input structure
-	nums []int16
-}
-
-//	FIXME read the input
-func readCaseInput(scanner *bufio.Scanner) caseInput {
-	size := readInt(scanner)
-	nums := make([]int16, size)
-	for i := range nums {
-		nums[i] = int16(readInt(scanner))
-	}
-	in := caseInput{nums}
-	return in
-}
-
-type caseOutput struct {
-	// FIXME test case output structure
-	equat int
-}
-
-func writeCaseOutput(writer *bufio.Writer, out caseOutput) {
-	//	FIXME write the out
-	writef(writer, "%d\n", out.equat)
-}
-
-func solveCase(in caseInput) caseOutput {
-	// FIXME actual solution
-	sum := 0
-	for _, v := range in.nums {
-		sum += int(v)
-	}
-	sum2 := int(0)
-	for i, v := range in.nums {
-		sum2 += int(v)
-		if sum2*2 >= sum {
-			return caseOutput{i + 1}
-		}
-	}
-	return caseOutput{len(in.nums)}
-}
-
 func solveSequential(scanner *bufio.Scanner, writer *bufio.Writer) {
 	n := readFloat64(scanner)
 	r := readFloat64(scanner)
@@ -84,33 +42,6 @@ func main() {
 	solveSequential(scanner, writer)
 }
 
-//	GoLang shorthand methods for I/O go below
-//	TODO wipe unused methods before submitting
-
-func readString(sc *bufio.Scanner) string {
-	if !sc.Scan() {
-		panic("failed to scan next token")
-	}
-
-	return sc.Text()
-}
-
-func readInt64(sc *bufio.Scanner) int64 {
-	if !sc.Scan() {
-		panic("failed to scan next token")
-	}
-
-	res, err := strconv.ParseInt(sc.Text(), 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	return res
-}
-
-func readInt(sc *bufio.Scanner) int {
-	return int(readInt64(sc))
-}
-
 func readFloat64(sc *bufio.Scanner) float64 {
 	sc.Scan()
 	res, err := strconv.ParseFloat(sc.Text(), 64)
@@ -126,49 +57,4 @@ func writef(writer *bufio.Writer, formatStr string, values ...interface{}) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-//	GoLang shorthand methods for math go below
-//	TODO wipe unused methods before submitting
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
-}
-
-func min64(a, b int64) int64 {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func max64(a, b int64) int64 {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func abs64(a int64) int64 {
-	if a < 0 {
-		return -a
-	}
-	return a
 }
