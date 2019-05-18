63080e1bd1b7d4af4afaf6ef7e76d007b2ee3742
akraievoy <akraievoy@gmail.com>
2019-05-18 11:11:46 +0100

    cf edu 63 a-c

---
+++ src/cf_edu63_a_reverse_substring/solution.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve(j Jam) {
+	n, s := j.Int(), []rune(j.Str())
+	for i := int64(1); i < n; i++ {
+		if s[i] < s[i-1] {
+			j.P("YES\n")
+			j.P("%d %d\n", i, i+1)
+			return
+		}
+	}
+	j.P("NO\n")
+}
+
+func main() {
+	jam, closeFunc := JamNew()
+	defer closeFunc()
+	solve(jam)
+}
+
+type Jam interface {
+	Scanner() *bufio.Scanner
+	Writer() *bufio.Writer
+	Close()
+
+	Str() string
+	Int() int64
+	Float() float64
+
+	P(format string, values ...interface{})
+	PF(format string, values ...interface{})
+}
+
+func JamNew() (Jam, func()) {
+	if len(os.Args) > 1 {
+		panic("running with input file path is not supported")
+	}
+
+	var scanner = bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Buffer(make([]byte, 0, 1024*1024), 1024*1024)
+
+	var writer = bufio.NewWriter(os.Stdout)
+	jam := &jam{scanner, writer}
+	return jam, jam.Close
+}
+
+type jam struct {
+	sc *bufio.Scanner
+	wr *bufio.Writer
+}
+
+func (j *jam) Close() {
+	if err := j.wr.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+func (j *jam) Scanner() *bufio.Scanner {
+	return j.sc
+}
+
+func (j *jam) Writer() *bufio.Writer {
+	return j.wr
+}
+
+func (j *jam) Str() string {
+	if !j.sc.Scan() {
+		panic("failed to scan next token")
+	}
+
+	return j.sc.Text()
+}
+
+func (j *jam) Int() int64 {
+	if !j.sc.Scan() {
+		panic("failed to scan next token")
+	}
+
+	res, err := strconv.ParseInt(j.sc.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
+
+func (j *jam) Float() float64 {
+	j.sc.Scan()
+	res, err := strconv.ParseFloat(j.sc.Text(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func (j *jam) P(format string, values ...interface{}) {
+	_, err := fmt.Fprintf(j.wr, format, values...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (j *jam) PF(format string, values ...interface{}) {
+	_, err := fmt.Fprintf(j.wr, format, values...)
+	if err != nil {
+		panic(err)
+	}
+	if err = j.wr.Flush(); err != nil {
+		panic(err)
+	}
+}
